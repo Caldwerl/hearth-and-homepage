7e72f6e56b90ce1ea185b015ca2320fff86b98b0
Caldwerl <caldwerl@gmail.com>
2014-09-04 23:43:27 -0700

    add neocron font and accordion to the article content

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,12 @@
 $(function () {
 
-  $('#content__article').accordion();
+  $('.content__article')
+    .accordion({
+      header: "h2",
+      active: true,
+      alwaysOpen: false,
+      fillspace: true,
+      collapsible: true,
+      heightStyle: 'content'
+    });
 });
